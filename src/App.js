91a3c189ef404a2970b68dd2df84499df3091858
SebastianOpiyo <sebamopale@gmail.com>
2021-08-31 17:52:13 +0300

    Entry file cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
 import Pages from './components';
-
-const URL = {
-  CATEGORIES: "https://api.chucknorris.io/jokes/categories",
-  JOKE: "https://api.chucknorris.io/jokes/random?category="
-}
 
 function App() {
   return (
